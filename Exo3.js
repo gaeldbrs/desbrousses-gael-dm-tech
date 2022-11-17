522db87249db47f453fc7b0510cd2c247a60ee1f
gael desbrousses <gael34070@hotmail.fr>
2022-11-17 19:22:32 +0100

    mise à jour exo3

--- Exo3.js
+++ Exo3.js
@@ -1,15 +1,10 @@
-Ecrire un algorithme qui demande un nombre de départ, et qui ensuite affiche les dix nombres suivants. Par exemple, si l'utilisateur entre le nombre 17, le programme affichera les nombres de 18 à 27.
-
-
-
-Variables N, i en Entier
+Variables
+   N , i : entier
 Debut
-Ecrire "Entrez un nombre : "
-Lire N
-Stop ← N+10
-Ecrire "Les 10 nombres suivants sont : "
-TantQue N < Stop
-   N ← N+1
-   Ecrire N
-FinTantQue
+   Ecrire("veuillez entrer un nombre : ")
+   Lire (N)
+   Pour i <-- N+1 à N+10 pas 1 faire
+      Ecrire(i)
+   fin Pour
+
 Fin
